8ab003d33a66be76a8c661fc4fb6fad049aff1d1
Tyuiu.BuynovaVI@outlook.com <v.buyanova1@mail.ru>
2025-01-24 17:56:27 +0500

    Task5

---
+++ Tyuiu.BuyanovaVI.Sprint6.Task5.V21/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BuyanovaVI.Sprint6.Task5.V21
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
