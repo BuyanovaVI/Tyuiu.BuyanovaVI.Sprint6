005fa4be9003afdd2fbab689a65927665b656768
Tyuiu.BuynovaVI@outlook.com <v.buyanova1@mail.ru>
2025-01-24 17:41:35 +0500

    Task4

---
+++ Tyuiu.BuyanovaVI.Sprint6.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BuyanovaVI.Sprint6.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BuyanovaVI.Sprint6.Task4.V1/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BuyanovaVI.Sprint6.Task4.V1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
