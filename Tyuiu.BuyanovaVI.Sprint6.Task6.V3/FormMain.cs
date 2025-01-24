88bf00737523488ce8d63a3991932e6b73330486
Tyuiu.BuynovaVI@outlook.com <v.buyanova1@mail.ru>
2025-01-24 23:07:45 +0500

    Task6

---
+++ Tyuiu.BuyanovaVI.Sprint6.Task6.V3/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BuyanovaVI.Sprint6.Task6.V3
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
